3b43b774d92abb97f8e0e4c528c934d2ca410d27
Singh <vish.jeea@gmail.com>
2018-08-18 21:15:25 -0700

    chart 1 using recharts

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { LineChart, Line, XAxis, YAxis, Tooltip, CartesianGrid, Label } from 'recharts';
+
+
+const data = [
+    {name: 'Jan', accountCount: 20},
+    {name: 'Feb', accountCount: 36},
+    {name: 'Mar', accountCount: 100},
+    {name: 'Apr', accountCount: 178},
+    {name: 'May', accountCount: 189},
+    {name: 'Jun', accountCount: 239},
+    {name: 'Jul', accountCount: 349},
+    {name: 'Aug', accountCount: 349},
+    {name: 'Sep', accountCount: 449},
+    {name: 'Oct', accountCount: 500},
+    {name: 'Nov', accountCount: 666},
+    {name: 'Dec', accountCount: 778},
+
+];
+
+class SimpleLineChart extends Component {
+    render () {
+        return (
+            <LineChart width={600} height={300} data={data}
+                       margin={{top: 5, right: 30, left: 20, bottom: 25}}>
+                <XAxis dataKey="name">
+                    <Label value="Youth accounts each month" offset={0} position="bottom" />
+                </XAxis>
+                <YAxis/>
+                <CartesianGrid strokeDasharray="3 3"/>
+                <Tooltip/>
+
+
+                <Line type="monotone" dataKey="accountCount" stroke="#82ca9d" />
+            </LineChart>
+        );
+    }
+}
+
+
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h2>Dashboard, current in dev mode</h2>
-    </div>
-  );
+    return (
+        <div id="container">
+            <SimpleLineChart/>
+        </div>
+    );
 };
 
 export default Dashboard;
